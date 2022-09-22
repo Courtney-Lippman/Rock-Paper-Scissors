92fd0b79a596f932a21be9d5bcb633685a8438a8
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-21 20:42:15 -0600

    Add more functionality.

--- src/player.js
+++ src/player.js
@@ -1,11 +1,28 @@
 class Player {
   constructor (name, token, wins) {
-    this.name: name || 'Human'
-    this.token: token || '&#x1f469'
-    this.wins: wins || 0
+    this.name = name
+    this.token = token
+    this.wins = wins || 0
+    this.fighter = 'rock'
   }
 
-  takeTurn() {
-
+  takeTurn (selectedFighter) {
+    this.fighter = selectedFighter ||
+      classicFightersList[Math.floor(Math.random() * 3)]
   }
 }
+
+// constructor - properties should include: name (ex: 'Human'), token (ex: '👩🏻'), wins (ex: 0)
+// saveWinsToStorage - only necessary if you choose the localStorage extension
+// retrieveWinsFromStorage - only necessary if you choose the localStorage extension
+// takeTurn
+var human = new Player('Human', '\uD83D\uDC69', 0)
+// Player {name: 'Human', token: '👩', wins: 0}
+var computer = new Player('Computer', '\uD83D\uDCBB', 0)
+// Player {name: 'Computer', token: '💻', wins: 0}
+// player1.takeTurn('scissors')
+// when invoked it switched rock to paper.
+// player1 // Player {name: 'Human', token: '👩', wins: 0, fighter: 'paper'}
+// player2.takeTurn('paper')
+// player 2 // Player {name: 'Computer', token: '💻', wins: 0, fighter: 'paper'}
+//player1.takeTurn() without an argument will then just generate a random fighter from the classicFightersList array in main.js
