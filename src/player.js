9cf7358308bd5ac36378fca80761d5566ea339b2
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-22 21:22:07 -0600

    Clean up player class and add more to takeTurn method to include difficult game info.

--- src/player.js
+++ src/player.js
@@ -5,24 +5,8 @@ class Player {
     this.wins = wins || 0
     this.fighter = 'rock'
   }
-
   takeTurn (selectedFighter) {
     this.fighter = selectedFighter ||
-      classicFightersList[Math.floor(Math.random() * 3)]
+      fightersList[Math.floor(Math.random() * fightersList.length)]
   }
 }
-
-// constructor - properties should include: name (ex: 'Human'), token (ex: '👩🏻'), wins (ex: 0)
-// saveWinsToStorage - only necessary if you choose the localStorage extension
-// retrieveWinsFromStorage - only necessary if you choose the localStorage extension
-// takeTurn
-var human = new Player('Human', '\uD83D\uDC69', 0)
-// Player {name: 'Human', token: '👩', wins: 0}
-var computer = new Player('Computer', '\uD83D\uDCBB', 0)
-// Player {name: 'Computer', token: '💻', wins: 0}
-// player1.takeTurn('scissors')
-// when invoked it switched rock to paper.
-// player1 // Player {name: 'Human', token: '👩', wins: 0, fighter: 'paper'}
-// player2.takeTurn('paper')
-// player 2 // Player {name: 'Computer', token: '💻', wins: 0, fighter: 'paper'}
-//player1.takeTurn() without an argument will then just generate a random fighter from the classicFightersList array in main.js
