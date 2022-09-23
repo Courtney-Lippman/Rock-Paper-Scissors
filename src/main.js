760d9dbbded83738cb4029528e54af0ef7a840e9
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-22 21:21:17 -0600

    Add select game functionality and include game instantiation along with creating fightersList variable.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,44 @@
-var classicFightersList = [ 'rock', 'paper', 'scissors' ]
-var difficultFightersList = ['rock', 'paper', 'scissors', 'alien', 'lizard']
+var fightersList
+var game = new Game('difficult')
+
+function selectFighterList () {
+  if (game.selectedGameType === 'classic') {
+    fightersList = [ 'rock', 'paper', 'scissors' ]
+  } else {
+    fightersList = [ 'rock', 'paper', 'scissors', 'alien', 'lizard' ]
+  }
+}
+selectFighterList()
+// game.human.takeTurn('scissors')
+// game.computer.takeTurn()
+// game.checkForWinner()
+// <'👩Human won this round!👩'
+// game.human.takeTurn('scissors')
+// game.computer.takeTurn()
+// game.checkForWinner()
+// <"😢It's a draw!😢"
+// game.human.takeTurn('scissors')
+// game.computer.takeTurn()
+// game.checkForWinner()
+// <'💻Computer won this round!💻'
+// game.human.takeTurn('scissors')
+// game.computer.takeTurn()
+// game.checkForWinner()
+// < '💻Computer won this round!💻'
+// game.human.takeTurn('scissors')
+// game.computer.takeTurn()
+// game.checkForWinner()
+// <"😢It's a draw!😢"
+// game.human.takeTurn('scissors')
+// game.computer.takeTurn()
+// game.checkForWinner()
+// <'💻Computer won this round!💻'
+// game.computer.wins
+// <4
+// game.human.wins
+// <1
+
+// To Play the Game in the Console:
+// game.human.takeTurn()
+// game.computer.takeTurn()
+// game.checkForWinner()
