cf38ffd1594fcaf48ccd872021a2c0fcd50eb606
Courtney Lippman <courtney.lippman@colorado.edu>
2022-09-25 15:09:55 -0600

    Change resetgame method.

--- src/game.js
+++ src/game.js
@@ -22,10 +22,3 @@ class Game {
   resetGameBoard () {
   }
 }
-// module.exports = Game
-// Two Player instances ✅
-// A way to keep track of the data for the game board ✅
-// A way to keep track of the selected game type ✅
-// A way to check the Game’s board data for win conditions ✅
-// A way to detect when a game is a draw (no one has won) ✅
-// A way to reset the Game’s board to begin a new game -- work in progress!
